e3125060c6a68011a7d90f78448ac27469508df7
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-05 12:09:29 +0100

    Send comment to event bus

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,9 @@
 const { randomBytes } = require('crypto');
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -10,7 +12,8 @@ const commentsByPostId = {};
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
@@ -19,7 +22,22 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  // Send event to event bus
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
