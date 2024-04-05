b89f4d8f0f5de9a187d759bac7a4aba7e9a5b3ca
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-05 14:51:04 +0100

    Use query service to get posts with comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,4 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const response = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(response.data);
-  };
-
-  useEffect(() => {
-    fetchComments().catch((err) => console.error(err));
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
